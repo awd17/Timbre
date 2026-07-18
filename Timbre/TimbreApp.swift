33705a85bc39094488d0a515f01f46d3a7cc1dcc
August Drakton <augdrak17@gmail.com>
2026-07-18 16:27:58 -0500

    Initial Commit

---
+++ Timbre/TimbreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimbreApp.swift
+//  Timbre
+//
+//  Created by August Drakton on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct TimbreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
